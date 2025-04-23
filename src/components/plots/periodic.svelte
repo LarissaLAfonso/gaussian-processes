91520e5beca0a8fc5ea1e2aa041acfb199f8658b
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-04-23 20:34:25 -0300

    add : Adicionei os gráficos defunções de Kernel

---
+++ src/components/plots/periodic.svelte
@@ -0,0 +1,16 @@
+<script>
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+    import { draw_kernel_func, kernel_Periodic, generateData } from '$components/plots/auxiliares.js';
+  
+    let svgContainer;
+    
+    const data = generateData(kernel_Periodic);
+    
+    onMount(() => {
+      draw_kernel_func(svgContainer, data);
+    });
+  </script>
+  
+  <div bind:this={svgContainer}></div>
+  
