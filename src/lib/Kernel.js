e5c5e2d457f754d71c4e33060b0e871821d6fb74
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-04-23 22:26:31 -0300

    fix : arrumados NaNs em vários Kernels

--- src/lib/Kernel.js
+++ src/lib/Kernel.js
@@ -16,7 +16,7 @@ export var Kernel = {
         y = tf.tensor(y);
         var diff = tf.sub(x, y);
         var dotDiff = tf.matMul(diff, diff, false, true);
-        var scaledDiff = tf.div(dotDiff, 2*tf.square(lengthScale));
+        var scaledDiff = tf.div(dotDiff, tf.square(2*lengthScale));
         return tf.exp(scaledDiff);
     },
     rationalQuadratic: function (x, y, alpha, lengthScale) {
@@ -30,4 +30,12 @@ export var Kernel = {
         console.log(scaledDiff.arraySync());
         return tf.pow(tf.add(1, scaledDiff), -alpha);
     },
+    periodic: function (x, y, period, lengthScale) {
+        x = tf.tensor(x);
+        y = tf.tensor(y);
+        var diff = tf.sub(x, y);
+        var dotDiff = tf.sqrt(tf.matMul(diff, diff, false, true));
+        var scaledDiff = tf.div(Math.PI*dotDiff, period);
+        return tf.exp(-2*lengthScale*tf.square(tf.sin(scaledDiff)));
+    }
 }
