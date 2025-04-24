e112a70b199dfca49d0dd092246500bc7e2bf03f
Larissa <larissahamlet@gmail.com>
2025-04-24 09:32:55 -0300

    changed bar icon

--- src/components/gaussian/Gaussian.Scrolly.svelte
+++ src/components/gaussian/Gaussian.Scrolly.svelte
@@ -49,6 +49,7 @@
 </script>
 
 <svelte:head>
+  <title>❥ Gaussian Processes </title>
   <script type="text/javascript" async
     src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
   </script>
