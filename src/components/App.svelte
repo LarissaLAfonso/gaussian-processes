c7834de76e328970a7c637269149d8f356a6ea22
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-04-26 16:55:10 -0300

    cleanup : comentários e elementos de teste removidos

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,22 +8,8 @@
     import mediaQueries from "$stores/mediaQueries.js";
     import { onMount } from "svelte";
     import * as tf from "@tensorflow/tfjs";
-    import MultivariateNormal from "multivariate-normal";
   
     let mounted = false;
-
-    var meanVector = [1, 2, 3];
-
-    // covariance between dimensions. This examples makes the first and third
-    // dimensions highly correlated, and the second dimension independent.
-    var covarianceMatrix = [
-        [ 1.0, 0.0, 0.9 ],
-        [ 0.0, 1.0, 0.0 ],
-        [ 0.9, 0.0, 1.0 ],
-    ];
-    
-    // var aa = MultivariateNormal(meanVector, covarianceMatrix);
-  
   
     onMount(() => { // Prevents the page from showing until the app is mounted
       mounted = true;
