1e8306cb57f8950605e7277fc79903ae9f3bfa01
Larissa <larissahamlet@gmail.com>
2025-04-29 02:42:47 -0300

    WIP: temporary save before applying stash

--- src/components/gaussian/Gaussian.svelte
+++ src/components/gaussian/Gaussian.svelte
@@ -1,8 +1,8 @@
 <script>
   import G1 from "$components/plots/grafico1.svelte";
   import G2 from "$components/plots/grafico2.svelte";
-  import G3 from "$components/plots/g3.svelte";
-  import G4 from "$components/plots/g4.svelte";
+  import G3 from "$components/plots/grafico3.svelte";
+  import G4 from "$components/plots/prior.svelte";
   import G5 from "$components/plots/matern.svelte";
   import G6 from "$components/plots/periodic.svelte";
   import G7 from "$components/plots/polinomial.svelte";
@@ -21,19 +21,15 @@
 {:else if activeStep === 2}
   <G2 />
 {:else if activeStep === 3}
-  <!-- <G3 /> -->
-  <ScatterPrior />
+  <G3 />
 {:else if activeStep === 4}
-  <!-- <G4 /> -->
-  <Prior />
-  {:else if activeStep === 5}
-  <!-- <G5 /> -->
-  <AllKernels />
-  {:else if activeStep === 6}
-  <!-- <G6 /> -->
-  <IndividualKernels />
+<G4 />
+{:else if activeStep === 5}
+<G5 />
+{:else if activeStep === 6}
+<G6 />
 {:else if activeStep === 7}
-  <!-- <G7 /> -->
-  {:else if activeStep === 8}
-  <!-- <G8 /> -->
+<G7 />
+{:else if activeStep === 8}
+<G8 />
 {/if}
