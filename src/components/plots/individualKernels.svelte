93253a64b92c56971eb879e91c5234d79ada0c43
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2025-05-04 22:34:48 -0300

    fix : linha de divisória removida

--- src/components/plots/individualKernels.svelte
+++ src/components/plots/individualKernels.svelte
@@ -65,16 +65,6 @@
             .attr("stroke", "#52DBA4") 
             .attr("stroke-width", 2)
             .attr("d", line);
-
-        svg.append("line")
-            .attr("x1", margin.left)
-            .attr("y1", height - margin.bottom)
-            .attr("x2", width - margin.right)
-            .attr("y2", height - margin.bottom)
-            .attr("stroke", "black")
-            .attr("stroke-width", 1)
-            .attr("opacity", 0.5)
-            .attr("stroke-dasharray", "5,5");  
         
         y.domain([0, d3.max(data, d => d.y)])
             .range([height - margin.bottom, margin.top]);
